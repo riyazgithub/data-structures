1d50dc506768b1113cc4937a2890534e0b2c9b0c
riyazgithub <sunez.riyaz@gmail.com>
2016-09-14 13:25:36 -0700

    Partially completed Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var front = 0 , rear = -1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    rear++;
+    storage[rear] = value;
+
   };
 
   someInstance.dequeue = function() {
+    var ret
+    if(front<=rear){
+      ret = storage[front]
+      front++
+    }
+
   };
 
   someInstance.size = function() {
+    return (rear-front+1)
   };
 
   return someInstance;
