8be55a78bf7b28c6ac1dddbabac97401a7ab8f56
riyazgithub <sunez.riyaz@gmail.com>
2016-09-14 13:21:54 -0700

    Stack Completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = -1;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function() {
+    var ret 
+    if(counter >= 0){
+      ret = storage[counter]
+      counter --;
+    }
+    return ret
   };
 
   someInstance.size = function() {
+    return (counter+1)
   };
 
   return someInstance;
