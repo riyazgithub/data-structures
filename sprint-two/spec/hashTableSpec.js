f0a1d9a0f440ad001394bf3e553c15d64b71d9f2
riyazgithub <h.a.riyaz@gmail.com>
2016-09-15 11:02:37 -0700

    Hashtable Partially completed.

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -40,6 +40,7 @@ describe('hashTable', function() {
     var v2 = 'val2';
     var oldHashFunction = window.getIndexBelowMaxForKey;
     window.getIndexBelowMaxForKey = function() { return 0; };
+    debugger;
     hashTable.insert(v1, v1);
     hashTable.insert(v2, v2);
     expect(hashTable.retrieve(v1)).to.equal(v1);
