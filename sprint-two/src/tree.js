8fab4b260ed20c50f737dcde2787ca72861834e2
riyazgithub <h.a.riyaz@gmail.com>
2016-09-15 09:35:15 -0700

    Completed Tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,35 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var treeNode = new Tree(value);
+  this.children.push(treeNode);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+  node = node || this;
+  var result = false;
+  for (var child = 0; child < node.children.length; child++) {
+    result = result || node.contains(target, node.children[child]);
+  }
+  if (node.value === target) {
+    return true;
+  }
+  return result;
 };
 
 
