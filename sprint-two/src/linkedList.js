e9f47d56eef2017f6686a5ac4e25848582bd79d9
riyazgithub <h.a.riyaz@gmail.com>
2016-09-14 19:55:20 -0700

    Completed Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if(!(this.tail && this.head)){
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var node = this.head;
+    if(this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+
+    return node.value;
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    while(node){
+      if(node.value === target){
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
