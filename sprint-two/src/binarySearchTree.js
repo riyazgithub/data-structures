6d5fb3490140a8110032f481d230667ccc25b24f
riyazgithub <h.a.riyaz@gmail.com>
2016-09-15 20:00:41 -0700

    Completed BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,23 +4,67 @@ var BinarySearchTree = function(value) {
   this.value = value;
 };
 
-BinarySearchTree.prototype.insert = function(value){
-  if(this === undefined){
+BinarySearchTree.prototype.inserthlpr = function(value, node){
+  /*if(node === null){
     return new BinarySearchTree(value)
   }
-  if(this.value > value){
-    this.left = insert(this.left);
+    if(node.value > value){
+    node.left = node.inserthlpr(value, node.left);
   } else {
-    this.right = insert(this.right);
+    node.right = node.inserthlpr(value, node.right);
+  }*/
+  if(node.value > value) {
+    if(node.left === null) {
+      node.left = new BinarySearchTree(value);
+    } else {
+      this.inserthlpr(value,node.left);
+    }
+
+  } else {
+    if(node.right === null) {
+      node.right = new BinarySearchTree(value);
+    } else {
+      this.inserthlpr(value,node.right);
+    }
   }
+
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  this.inserthlpr(value,this);
+  //debugger;
+};
+
+BinarySearchTree.prototype.containshlpr = function(value, node){
+  var result = false;
+  if(node === null){
+    return false;
+  }
+  if(node.value > value){
+    result = result || this.containshlpr(value, node.left);
+  } else if(node.value < value) {
+    result = result || this.containshlpr(value, node.right);
+  } else if(node.value === value){
+    return true;
+  }
+  return result;
 };
 
 BinarySearchTree.prototype.contains = function(value){
-
-};
+  return this.containshlpr(value, this);
+}
 
 BinarySearchTree.prototype.depthFirstLog = function(cb){
+  this.depthFirstLoghlpr(cb, this);
+};
 
+BinarySearchTree.prototype.depthFirstLoghlpr = function(cb, node){
+  if(node === null){
+    return;
+  }
+  cb(node.value); 
+  this.depthFirstLoghlpr(cb, node.left);
+  this.depthFirstLoghlpr(cb, node.right);
 };
 
 /*
